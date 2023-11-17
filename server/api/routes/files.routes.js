02db6639c06e6932d22eb48dcebb2183ba3b1a24
KieKoongDev <kiekoong.dev@gmail.com>
2023-11-17 11:43:33 +0700

    gggg

--- server/api/routes/files.routes.js
+++ server/api/routes/files.routes.js
@@ -1,18 +1,69 @@
-const route = require('express').Router()
+const route = require('express').Router();
+const { google } = require('googleapis');
+const multer = require('multer'); 
+const key = require('../../../cred.json');
+const path = require('path')
+const fs = require('fs')
+const Seller = require('../models/UsersModel/SellersModel.js')
 
-const files = require('../controllers/uploadfile.js')
+const auth = new google.auth.GoogleAuth({
+  credentials: {
+    client_email: key.client_email,
+    private_key: key.private_key,
+  },
+  scopes: ['https://www.googleapis.com/auth/drive'],
+});
 
-const multer = require('multer')
+const drive = google.drive({version: 'v3', auth});
 
-let storage = multer.diskStorage({
-    destination: './uploads',
-    filename: (req, file, callback) => {
-        const extension = file.originalname.split(".").pop()
-        callback(null, `${file.fieldname}-${Date.now()}.${extension}`)
+const upload = multer({ dest: 'uploads/' }); // Adjust destination folder as needed
+
+route.post('/', upload.any(), async (req, res) => {
+  try{
+
+    console.log(req.body);
+    console.log(req.files)
+
+    const {body, files} = req
+
+    for(let f=0 ; f<files.length ; f++){
+      await uploadFile(files[f])
     }
-})
-const upload = multer({ storage: storage })
 
-route.post('/', upload.single('file'), files.uploadFile)
+    res.send('form submitted')
+  }
+  catch(err){
+    console.log(err)
+  }
+});
+
+const uploadFile = async (fileObjects) => {
+  try{
+  const {data} = await drive.files.create({
+      media : {
+        mimeType: fileObjects.mimetype,
+        body: fs.createReadStream(path.join(__dirname, '..', '..', fileObjects.path))
+      },
+      requestBody : {
+        name: fileObjects.originalname,
+        parents: ["1C_GASrUjSus7uDC4-_VQWpuYIfistQe7"]
+      },
+      fields : 'id, name'
+  })
+
+  fs.unlink(path.join(__dirname, '..', '..', fileObjects.path), (err)=>{
+    if(err){
+        console.log('unlink error')
+    } else {
+        console.log('unlink success')
+    }
+  })
+
+  console.log(`Upload file ${data.name} ${data.id}`)
+  }
+  catch(err){
+    console.log(err)
+  }
+}
 
 module.exports = route
