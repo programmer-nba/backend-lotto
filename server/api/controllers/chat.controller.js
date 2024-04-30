c189090236477c9b90d4ad08e8410f068d8181f5
KieKoongDev <kiekoong.dev@gmail.com>
2024-04-30 11:52:08 +0700

    ss

--- server/api/controllers/chat.controller.js
+++ server/api/controllers/chat.controller.js
@@ -111,6 +111,33 @@ exports.getChat = async (req, res) => {
     }
 }
 
+exports.getChats = async (req, res) => {
+    const { id } = req.params;
+    try {
+    const chat = await Chat.find({
+        'members.$._id': id
+    });
+
+    if (!chat) {
+        return res.status(404).json({
+            message: "Chat not found."
+        });
+    }
+
+    return res.status(200).json({
+        message: "Chat found successfully.",
+        chat: chat
+    })
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            message: err.message,
+            err: err
+        });
+    }
+}
+
 exports.sendMessage = async (req, res) => {
     const { id } = req.params // chat_id
     const img = req.files
