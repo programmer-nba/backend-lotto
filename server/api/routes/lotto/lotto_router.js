c33076d3b4cff640f7674666ad134c05d9170ea4
KieKoong <kiekoong.dev@gmail.com>
2024-09-01 23:51:41 +0700

    dddd

--- server/api/routes/lotto/lotto_router.js
+++ server/api/routes/lotto/lotto_router.js
@@ -2,6 +2,7 @@ const router = require("express").Router()
 const LottoWholesale = require("../../controllers/lotto/lotto_controller")
 
 router.post("/lottos-wholesale", LottoWholesale.createLottosWholesale)
+router.put("/lottos-wholesale/:id", LottoWholesale.updateLottoWholesale)
 router.get("/lottos-wholesale", LottoWholesale.getLottosWholesale)
 router.get("/lottos-wholesale/:id", LottoWholesale.getLottoWholesale)
 
